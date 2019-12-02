4954001f41d2bac56be980e0971807920dd387f0
lauraherrero <lauraherrero36@gmail.com>
2019-12-02 15:49:33 +0100

    First commit and add indfindex, style and main js

---
+++ _src/assets/js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//Declaro los elementos que voy a necesitar
+const elementInputSearch = document.querySelector('#inputSearch');
+const elementButton = document.querySelector('#buttonSearch');
+const elementContainer = document.querySelector('#seriesContainer');
+
+const urlBase = 'http://api.tvmaze.com/search/shows?q=girls';
