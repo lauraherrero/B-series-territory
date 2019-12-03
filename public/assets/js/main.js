6940e6c1d56f2b3fe8daf838d5b547c5ef1e2a7a
lauraherrero <lauraherrero36@gmail.com>
2019-12-03 20:28:49 +0100

    Add new functions for paint series, click favourites, and add favourites

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -13,7 +13,7 @@ const urlBase = 'http://api.tvmaze.com/search/shows?q=';
 //Declaro en una variable mi array vacío
 let favouriteSeries = [];
 
-
+//Función para conectarse a la Api
 const connectToApi = () => {
 const inputValue = elementInputSearch.value.toLowerCase();
   fetch(urlBase + inputValue)
@@ -27,10 +27,10 @@ function loadSeriesHandler () {
     favouriteSeries = JSON.parse(localStorage.getItem(allFavSeries));
     addNewFavourite(favouriteSeries);
   } else {
-    paintList();
+    paintSeries();
     connectToApi();
-  }
-}
+  };
+};
 
 
 //Creo una función que pinte las series con los datos que recojo de la API.
@@ -38,97 +38,89 @@ const paintSeries = (dataSearch) => {
   for (let i = 0; i < dataSearch.length; i++) {
     const serieName = dataSearch[i].show.name;
     const serieImage = dataSearch[i].show.image;
-
     const elementDiv = document.createElement ('div');
     const elementImg = document.createElement('img');
     const elementSpan = document.createElement('span');
     const spanText = document.createTextNode(serieName);
-    
-    elementImg.style = 'width: 100px';
+    elementDiv.classList.add('serie-content');
+    elementImg.classList.add('serie-img');
+    elementSpan.classList.add('serie-text');
 
-    //Necesito un condicional para la imagen. En caso de que no exista, por defecto píntame esto, sino, pintame la imagen que me traigo de la api (mediano u original).
+    //Necesito un condicional para la imagen. En caso de que no exista, por defecto píntame esto, sino, pintame la imagen que me traigo de la api.
     if(serieImage === null){
       elementImg.src = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
     } else {
-      elementImg.src = serieImage.original || serieImage.medium;
-    }
+      elementImg.src = serieImage.original;
+    };
 
     elementSpan.appendChild(spanText);
-    elementDiv.appendChild(elementSpan);
     elementDiv.appendChild(elementImg);
+    elementDiv.appendChild(elementSpan);
     elementContainer.appendChild(elementDiv);    
     
     //Al pinchar sobre el div que contiene la imagen y el nombre de la serie, tenemos que añadir esta a favoritos.
-    elementDiv.addEventListener('click', addNewFavourite);
-
-    const seriesObject = {
-      "name" : serieName,
-      "image" : serieImage,
-    }
-    favouriteSeries.push(seriesObject);
-
+    elementDiv.addEventListener('click', clickFavourite);
   };
-  
-}
-
-const paintList = (array) => {
-  for(let item of array) {
-    addNewFavourite(item)
-  }
-}
-
-const addNewFavourite = (obj) =>{
-  const favouriteLi = document.createElement('li');
-  favouriteLi.innerHTML = `<img src=${obj.img}><span>${obj.name}</span>`;
-  elementFavouriteList.appendChild(favouriteLi);
-
-  
-
-  localStorage.setItem("allFavSeries",JSON.stringify(favouriteSeries));
-
 };
 
-  // for(let item of favouriteSeries){
-  //   const favImgData = item.img;
-  //   const favNameData = item.name;
 
-  //   const favouriteLi = document.createElement('li');
-  //   const favouriteName = document.createElement('span');
-  //   const favouriteImage = document.createElement('img');
-  //   favouriteName.classList.add('fav-name');
-  //   favouriteImage.classList.add('fav-image');
-  //   favouriteLi.classList.add('item');
-  //   favouriteImage.src = favImgData;
-  //   favouriteName.innerHTML = favNameData;
-  //   favouriteLi.appendChild(favouriteImage);
-  //   favouriteLi.appendChild(favouriteName);
-  //   elementFavouriteList.appendChild(favouriteLi);
-    
 
-    
-    // const newElement = event.currentTarget;
-    // const selectSpan = newElement.querySelector('.fav-name');
-    // const selectImg = newElement.querySelector('.fav-image');
-    // newElement.classList.toggle('color-brackground');
-    // if(newElement.classList.contains('color-brackground')){
-    //   const seriesObject = {
-    //     "name" : selectSpan.innerHTML,
-    //     "img" : selectImg.src,
-    //   }
-    // } 
+//Función para añadir o quitar el fondo de la serie cuando hago click en el div
+function clickFavourite (event) {
+  const selectSerie = event.currentTarget;
+  selectSerie.classList.toggle('click-select');
+  const image =  selectSerie.firstChild.src;
+  const name = selectSerie.lastChild.innerHTML;
+ 
+  //Declaro mi objeto
+  const seriesObject = {
+    "name": name,
+    "image": image,
+  };
   
-    
+  //Condicional para que añada el objeto al array cuando tiene la clase seleccionada y la añada a favoritos.
+  if(selectSerie.classList.contains('click-select')){
+    favouriteSeries.push(seriesObject);
+  };
+  addNewFavourite();
+  
+  //Guardo en localStorage mi array.
+  localStorage.setItem("allFavSeries",JSON.stringify(favouriteSeries));
+};
 
-// const addNewSerie = (seriesObject) =>{
-//   elementFavouriteList.innerHTML = `<img src=${seriesObject.Image}><span>${seriesObject.Name}</span>`
-// }
+//Función para que al añadir la serie me añada imagen y título en favoritos.
+const addNewFavourite = () =>{
+  
+  elementFavouriteContainer.innerHTML = '';
+  elementFavouriteList.innerHTML = '';
+  elementFavouriteList.classList.add('favourite-list');
+  elementFavouriteContainer.appendChild(elementFavouriteList);
 
-//Función para hacer la búsqueda cuando pulso enter
-const submitHandler = (event) => {
-  event.preventDefault();
-  connectToApi();
-}
+  for(let item of favouriteSeries){
+    const favName = item.name;
+    const favImage = item.image;
 
-elementForm.addEventListener('submit', submitHandler);
+    const favouriteLi = document.createElement('li');
+    const nameFavourite = document.createElement('span');
+    const imageFavourite = document.createElement('img');
+    const closeFavourite = document.createElement('span');
+    const nameContent = document.createTextNode(favName);
+    closeFavourite.innerHTML = 'X';
+    closeFavourite.classList.add('icon');
+    nameFavourite.classList.add('favourite-name');
+    imageFavourite.classList.add('favourite-image');
+    favouriteLi.classList.add('favourite-content');
+    imageFavourite.src = favImage;
+
+    nameFavourite.appendChild(nameContent);
+    favouriteLi.appendChild(imageFavourite);
+    favouriteLi.appendChild(nameFavourite);
+    favouriteLi.appendChild(closeFavourite);
+    elementFavouriteList.appendChild(favouriteLi);
+  };
+}; 
+
+  
+//Ejecuto mi listener sobre el elemento, en este caso el botón para que genere la función de conectarse a la Api.
 elementButton.addEventListener('click', connectToApi);
 //# sourceMappingURL=main.js.map
